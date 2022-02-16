26dc2ab168df9e1b58bfb979cd40a2a50d690943
Chucky-GitHub <79621707+Chucky-GitHub@users.noreply.github.com>
2022-02-16 04:18:45 +0300

    Update ME461Group.py

--- ME461Group.py
+++ ME461Group.py
@@ -94,30 +94,30 @@ class ME461Group:
         # a very simple randomizer
         maxL = self.maxStep # total travel
         
-#         self.reduced = self.compress(img)[:,:,0:3]
+        self.reduced = self.compress(img)[:,:,0:3]
 
-#         pos_dict = {}
+        pos_dict = {}
 
-#         y,x = np.where(np.all(self.reduced==self.colorzzz['clr20'][0][0:3],axis=2))
-#         pos_dict[20] = np.column_stack((y,x))
-#         y,x = np.where(np.all(self.reduced==self.colorzzz['clr30'][0][0:3],axis=2))
-#         pos_dict[30] = np.column_stack((y,x))
-#         y,x = np.where(np.all(self.reduced==self.colorzzz['clr50'][0][0:3],axis=2))
-#         pos_dict[50] = np.column_stack((y,x))
-#         y,x = np.where(np.all(self.reduced==self.colorzzz['clr100'][0][0:3],axis=2))
-#         pos_dict[100] = np.column_stack((y,x))
+        y,x = np.where(np.all(self.reduced==self.colorzzz['clr20'][0][0:3],axis=2))
+        pos_dict[20] = np.column_stack((y,x))
+        y,x = np.where(np.all(self.reduced==self.colorzzz['clr30'][0][0:3],axis=2))
+        pos_dict[30] = np.column_stack((y,x))
+        y,x = np.where(np.all(self.reduced==self.colorzzz['clr50'][0][0:3],axis=2))
+        pos_dict[50] = np.column_stack((y,x))
+        y,x = np.where(np.all(self.reduced==self.colorzzz['clr100'][0][0:3],axis=2))
+        pos_dict[100] = np.column_stack((y,x))
 
-#         biz = [int(y1/50),int(x1/50)]
+        biz = [int(y1/50),int(x1/50)]
 
-#         yenimesafe = float('inf')
-#         for i in pos_dict.keys():
-#             for j in pos_dict[i]:
-#                 mesafe = abs(biz[0]-j[0])+abs(biz[1]-j[1])
-#                 if mesafe <= yenimesafe:
-#                     hedef = j
-#                     yenimesafe = mesafe
+        yenimesafe = float('inf')
+        for i in pos_dict.keys():
+            for j in pos_dict[i]:
+                mesafe = abs(biz[0]-j[0])+abs(biz[1]-j[1])
+                if mesafe <= yenimesafe:
+                    hedef = j
+                    yenimesafe = mesafe
 
-#         print('bizim yer',biz,'- hedef',hedef)
+        print('bizim yer',biz,'- hedef',hedef)
 
 #         tumiht = []
 #         yollar = []
