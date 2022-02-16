73de8d882fc8f0947d9246f26a251aeadb03508a
Chucky-GitHub <79621707+Chucky-GitHub@users.noreply.github.com>
2022-02-16 03:10:12 +0300

    Update ME461Group.py

--- ME461Group.py
+++ ME461Group.py
@@ -49,4 +49,4 @@ class ME461Group:
             nx2 = nx - dx2       
         # right before return wait a certain amount of time
         time.sleep(self.maxTime * np.random.rand())
-        return [[y+131,x]]
+        return [[y+33,x]]
